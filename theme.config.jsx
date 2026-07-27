1c8b48032175cd8eb4e3f3d48489d8d31a4c735a
Siwon Kim <116896859+ctsiwonnim@users.noreply.github.com>
2026-07-27 16:13:44 +0900

    feat: config 파일 추가

---
+++ theme.config.jsx
@@ -0,0 +1,7 @@
+export default {
+  logo: <span style={{ fontWeight: 700, color: '#2F6E4E' }}>김시원 포트폴리오</span>,
+  project: { link: 'https://github.com/ctsiwonnima' },
+  footer: { text: '© 2026 김시원' },
+  primaryHue: 152,
+  primarySaturation: 40,
+}
